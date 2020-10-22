3c97f70d0fe5daa37e640007b13729d8c8141f34
chibuisi <enyia21@gmail.com>
2020-10-21 19:33:50 -0500

    database_updated

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,5 +1,5 @@
 class Team < ApplicationRecord
-  has_many :team_superheros
+  has_many :team_superheros, dependent: :destroy
   has_many :superheros, through: :team_superheros
   belongs_to :user
   
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    has_many :teams
+    has_many :teams, dependent: :destroy
     has_many :team_superheros, through: :teams
     has_many :superheros, through: :team_superheros
 
